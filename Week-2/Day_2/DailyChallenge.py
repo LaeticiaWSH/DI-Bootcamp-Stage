4101809e257cf5be07730b02466ef5534e823bf8
LaeticiaWSH <laeticiaariana@gmail.com>
2023-07-19 13:44:18 +0400

    updates

--- Week-2/Day_2/DailyChallenge.py
+++ Week-2/Day_2/DailyChallenge.py
@@ -8,12 +8,33 @@
 # print(list)
 
 #Challenge 2
-list = []
-word = input("Enter a word :")
-list = word.split
-for x in range(len(word)):
-    print(x)
-    if word[x] == word[x + 1]:
-        del list[x]
+# list = []
+# word = input("Enter a word :")
+# w = ' ' .join(word)
+# list.append (w.split())
+# for x in range(len(word)):
+#     print(x)
+#     # if word[x] == word[x + 1]:
+#     #     l= word[x]
+#     #     i = word[x + 1]
+#     # print(l)
+#     # list.append(l)
+#     if list[x] == list[x + 1]:
+#         list.remove[x]
 
-print(list)
+# print(list)
+
+word = input("Enter a word: ")
+i = 0
+j = 0
+correctword = " "
+while (j < len(word)):
+    if word[i] == word[j]:
+        j+=1
+    elif ((word[j]!= word[i]) or (j == len(word) - 1)):
+        correctword += word[i]
+        i = j
+        j += 1
+
+correctword += word[j - 1]
+print(correctword)
