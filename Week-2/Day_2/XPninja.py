c38c6a1f4c5a2b8933c74e06ca427efa0736a4c2
LaeticiaWSH <laeticiaariana@gmail.com>
2023-07-27 22:47:35 +0400

    file updates

---
+++ Week-2/Day_2/XPninja.py
@@ -0,0 +1,42 @@
+import math
+import numpy as maths
+##-------ERROR  ERROR---------
+# # # # Ex 1
+# num = input("Enter a comma seperated string of numbers ")
+# D = num.split(",")
+# print(D)
+# list =[]
+# for i in D:
+#      C = 50
+#      H = 30
+#      j = int(i)
+#     #  list.append(j)
+#      print(j)
+#      Q = maths.sqrt([(2 * C * j)/H])
+#      list.append(Q)
+#      print(Q)
+# array = (maths.sqrt(list))
+# arr = list(map(int,Q))
+# print(arr)
+# print(list)
+## -------THIS IS JUST A REMINDER OF THE HARD TIMES IN LIFE---------
+
+
+num = input("Enter a comma seperated string of numbers ")
+Slist = num.split(",")
+D = list(map(int,Slist))
+print(D)
+list =[]
+for i in D:
+     C = 50
+     H = 30
+     print(i)
+     Q = (2 * C * i)/H
+     list.append(Q)
+print(Q)
+list = maths.sqrt(list)
+list = [int(x) for x in list]
+list = [str(x) for x in list]
+print(list)
+l = ','.join(list)
+print(l)
