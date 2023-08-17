d05433f4d17b8c4a2ad9b31b5c089bf41b39c840
Laeticia Laeticiaoceane Océane <laeticiaariana@gmail.com>
2023-08-17 15:35:17 +0400

    exe

---
+++ Week-6/Day_4/dailychallenge2.js
@@ -0,0 +1,17 @@
+// //First part
+let x = "* "
+// let len = prompt("Enter your number: ")
+// for (let i = 0;i < len;i++){
+//     console.log(x.repeat(i))
+// }
+
+//Second part 
+let len = 6
+for (let i = 1; i < 2; i++) {
+    pattern = ""
+    for (let j = 0; j < len; j++){
+        pattern += " *"
+        console.log(pattern)
+    }
+}
+
