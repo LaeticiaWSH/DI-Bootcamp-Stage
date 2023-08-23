135da09ebee4396c9a8c61bd2f8289142a0b3875
Laeticia Laeticiaoceane Océane <laeticiaariana@gmail.com>
2023-08-23 10:52:52 +0400

    DailyChallengeWeek2Day3

--- Week-2/Day_3/DailyChallenge.py
+++ Week-2/Day_3/DailyChallenge.py
@@ -1,43 +1,68 @@
 
 # #Daily Challenge 1
-# word = input('Enter a word : ')
-# a = word.lower()
-# dict = {x : [] for x in a}   #creates dict with keys being unique values in a
-# for i,x in enumerate(a):
-#     dict[x].append(i) 
+word = input('Enter a word : ')
+a = word.lower()
+dict = {x : [] for x in a}   #creates dict with keys being unique values in a
+for i,x in enumerate(a):
+    dict[x].append(i) 
 
-# print(dict)
+print(dict)
 
 #  #Daily Challenge 2
-items_purchase = {
-  "Water": '$1',
-  "Bread": '$3',
-  "TV": '$1,000',
-  "Fertilizer": '$20'
+items_purchase1 = {
+  "Water": "$1",
+  "Bread": "$3",
+  "TV": "$1,000",
+  "Fertilizer": "$20"
 }
-# mapped_dict = {key :value.strip('$') for key,value in items_purchase()}
-# print(mapped_dict)
-# # def convert_to_int(v):
-#     result = [dict([v, int(x)] for k, v in items_purchase.items())]
-#     return result
 
-# # list = []
-# wallet = (input('Enter the money you have $$ = '))
-# # # price = items_purchase1.values.replace('$','')
-# # # items_purchase = [{k : int(v) for k, v in items_purchase1.items()}]
-# def update(v):
-for k,v in items_purchase.items():
-        price = (v.strip('$'))
-        items_purchase[k] = (price)
-print(items_purchase)
-#items_purchase = {k : int(v) for k, v in items_purchase.items()}
-#         print(v)
-#x = map(update([k, int(v)] for k, v in items_purchase.items()))
-# #     v = v.replace('$','')
-# #     price = convert_to_int(v)
-# #     print(type(price))
-# print(x)
-# # #     if wallet > price :
-# # #         list.append(k)
-# # print(list)
+# Expected output if wallet = "$300"
+ #["Bread", "Fertilizer", "Water"]
+
+items_purchase2 = {
+  "Apple": "$4",
+  "Honey": "$3",
+  "Fan": "$14",
+  "Bananas": "$4",
+  "Pan": "$100",
+  "Spoon": "$2"
+}
+
+#Expected output if wallet = "$100" 
+# ["Apple", "Bananas", "Fan", "Honey", "Pan", "Spoon"]
+
+items_purchase3 = {
+  "Phone": "$999",
+  "Speakers": "$300",
+  "Laptop": "$5,000",
+  "PC": "$1200"
+}
+
+# Expected output if wallet = "$1" 
+#"Nothing"
+
+item_purchasedNo = int(input("Enter the items_purchased your interested in (1-3): "))
+
+if 0 < item_purchasedNo < 4:
+    if item_purchasedNo == 1:
+      items_purchased = items_purchase1
+    elif item_purchasedNo == 2:
+      items_purchased = items_purchase2
+    else:
+      if item_purchasedNo == 3:
+        items_purchased = items_purchase3
+  
+
+my_wallet = int(input("Enter the amount in your wallet: "))
+can_afford = []
+
+for item, price in items_purchased.items():
+   item_price = int(price.replace("$","").replace(",",""))
+   if item_price <= my_wallet:
+      can_afford.append(item)
+if can_afford != []:
+  can_afford = sorted(can_afford)   
+  print(can_afford)
+else:
+   print("You can afford Nothing.")
 
