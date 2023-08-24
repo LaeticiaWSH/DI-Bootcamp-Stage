5bf0c4612accb3a028f2ed2ee55098f3741bdcda
Laeticia Laeticiaoceane Océane <laeticiaariana@gmail.com>
2023-08-24 20:23:54 +0400

    week7day3exerciseXP

--- Week-7/Day_3/exerciseXP.js
+++ Week-7/Day_3/exerciseXP.js
@@ -77,42 +77,46 @@ let Checktype = (a) =>{
 console.log(Checktype("ABCD"))
 console.log(Checktype(1234))
 
-// //ex 4 
+//ex 4 
  let sum = (a,b)=> a + b;
  console.log(sum(1,2))
 
 //ex 5
 //First, use function declaration and invoke it.
 function kgToGramDeclaration(weight){
-    return weight * 1000
+    return weight * 1000;
 }
+// console.log(kgToGramDeclaration(2))
+// //Then, use function expression and invoke it.
+const kgToGram = function(weight){
+    return weight * 1000;
+}
+// console.log(kgToGram(4))
+//Finally, use a one line arrow function and invoke it.
+const ktg = w =>  w * 1000;
+console.log(ktg(6))
 
-let ktg = (w) =>{return w * 1000}
-console.log(ktg)
 
 
 //ex 6
+//Create a self invoking function that takes 4 arguments
 (function(numberofchildren, partnersname , geographiclocation, jobtitle){
     let sentence = `You will be a ${jobtitle} in ${geographiclocation}, and married to ${partnersname} with ${numberofchildren} kids.`
+    //The function should display in the DOM a sentence
     document.getElementById("container").textContent = sentence
 })(3,"Chase","Portland","Web-developer");
-// //ex 7
+//ex 7
+//Create a Navbar in your HTML file.
+//In your js file, create a self invoking funtion that takes 1 argument: the name of the user that just signed in.
 (function(){
     let n = prompt("Hello! what's your name? ")
     document.getElementById("navbar").textContent = "Welcome " + n
 })();
 //ex 8
-function triangleHypotenuse(base,height)
-{
-function square(side){
-return side*side;
-}
-return Math.sqrt(square(base)+square(height));
-}
-document.write("Hypotenuse of triangle is : "+triangleHypotenuse(3,4));
-
+// The outer function named makeJuice receives 1 argument: the size of the beverage the client wants - small, medium or large.
 function makeJuice(size)
 {
+    //The inner function named addIngredients receives 3 ingredients, and displays on the DOM a sentence 
 function addIngredients(a,b,c){
 return `The client wants a ${size} juice, containing ${a}, ${b}, ${c}`;
     }
@@ -122,8 +126,12 @@ let s = prompt("Enter the desired beverage size(large/medium/small) : ")
 document.write(makeJuice(s))
 
 
+// -----Part II-----
+//In the makeJuice function, create an empty array named ingredients.
 function makeJuice(size){
     let ingredients = [];
+    //The addIngredients function should now receive 3 ingredients, and push them into the ingredients array
+
     function addIngredients(a,b,c){
         ingredients.push(a);
         ingredients.push(b);
@@ -132,6 +140,7 @@ function makeJuice(size){
 addIngredients("apple","ice","refreshing water");
 addIngredients("kiwi","sugar","syrup");
 
+//Create a new inner function named displayJuice that displays on the DOM a sentence
     function displayJuice(){
         return `The client wants a ${size} juice, containing ${ingredients.join(' ,')}.`;
         }
@@ -139,4 +148,4 @@ return displayJuice()
 
 }
 let f = prompt("Enter the desired beverage size(large/medium/small) : ")
-document.write(makeJuice(s))
+document.write(makeJuice(f))
